9ad6e790aa00702f98e6b479552fae337cbe2261
Thomas Klein <thomas.klein.1982@gmail.com>
2026-07-17 01:18:11 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,47 @@ export default defineConfig({
           }
         }
       }
+    }),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'info-trainer',
+        short_name: 'info-trainer',
+        description: 'Lerne Informatik und das Programmieren mit Java.',
+        theme_color: '#121212',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico,woff2,eot,ttf,woff,wasm}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
     })
   ],
   base: "./"
