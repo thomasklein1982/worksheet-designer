abe33d118b20ba45e27ef2dba33cc0c6a3946f4e
Thomas Klein <thomas.klein.1982@gmail.com>
2026-08-11 05:42:14 +0200

    kopfzeile klappt wieder

--- src/ab-components/seite.js
+++ src/ab-components/seite.js
@@ -14,7 +14,7 @@ export default{
     for(let i=0; i<templates.length;i++){
       tag=scope.templates["kopfzeile-"+templates[i]];
       if(tag){
-        let code=parseNode(scope.code,tag,scope,true).code;
+        let code=parseNode(scope.code,tag,scope,true);
         open+=code;
       }
     }
@@ -63,7 +63,7 @@ export default{
     for(let i=0; i<templates.length;i++){
       tag=scope.templates["fusszeile-"+templates[i]];
       if(tag){
-        let code=parseNode(scope.code,tag,scope,true).code;
+        let code=parseNode(scope.code,tag,scope,true);
         close+=code;
       }
     }
